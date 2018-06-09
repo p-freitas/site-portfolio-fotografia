188bb0877ce962a5e628e34325c0a9a775533693
SrKing <pedro.pauloy@outlook.com>
2018-06-09 17:45:37 -0300

    primeiro commit

---
+++ assets/js/menu.js
@@ -0,0 +1,16 @@
+/*GainTime v3.0.0 - CourageousWarrior - Modern*/
+var header = document.getElementById('menu');
+
+var el = document.scrollingElement || document.documentElement;
+if (el.scrollTop >= 5) {
+    header.setAttribute("class", "gt-top-menu sm-left fixed white-bg")
+}
+document.addEventListener("scroll", function() {
+    if (el.scrollTop >= 5) {
+      header.classList.remove("transparent")
+      header.classList.add("white-bg")
+    } else {
+      header.classList.add("transparent")
+      header.classList.remove("white-bg")
+    }
+});
